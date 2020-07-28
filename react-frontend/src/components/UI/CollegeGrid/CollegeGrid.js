e39e6a7156446dcf9705e739a27de8202c2fa142
Jacky Xu <jackyxu8832@gmail.com>
2020-07-28 15:25:29 -0400

    detail card styling adjust, delete icons

--- react-frontend/src/components/UI/CollegeGrid/CollegeGrid.js
+++ react-frontend/src/components/UI/CollegeGrid/CollegeGrid.js
@@ -2,11 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import AutorenewIcon from "@material-ui/icons/Autorenew";
-import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
-import ChildCareIcon from "@material-ui/icons/ChildCare";
-import FaceIcon from "@material-ui/icons/Face";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSchool, faUserGraduate } from "@fortawesome/free-solid-svg-icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,13 +26,11 @@ const CollegeGrid = (props) => {
       <Grid container spacing={3}>
         <Grid item xs>
           <Paper className={classes.paper}>
-            <ArrowForwardIcon color="primary" />
-            Transfer rate: {(props.college.transfer_rate * 100).toFixed(2)} %
+            Transfer-out rate: {props.college.transfer_rate === 0 ? "N/A" : `${(props.college.transfer_rate * 100).toFixed(2)}%`}
           </Paper>
         </Grid>
         <Grid item xs>
           <Paper className={classes.paper}>
-            <AutorenewIcon color="secondary" />
             Retention rate: {(props.college.retention_rate * 100).toFixed(2)} %
           </Paper>
         </Grid>
@@ -43,13 +38,11 @@ const CollegeGrid = (props) => {
       <Grid container spacing={3}>
         <Grid item xs>
           <Paper className={classes.paper}>
-            <AttachMoneyIcon color="primary" />
             In-state tuition: $ {props.college.tuition_IN}
           </Paper>
         </Grid>
         <Grid item xs>
           <Paper className={classes.paper}>
-            <AttachMoneyIcon color="secondary" />
             Out-state tuition: $ {props.college.tuition_OUT}
           </Paper>
         </Grid>
@@ -57,14 +50,14 @@ const CollegeGrid = (props) => {
       <Grid container spacing={3}>
         <Grid item xs>
           <Paper className={classes.paper}>
-            <ChildCareIcon color="primary" />
-            Undergraduates: {props.college.enrollment}
+            <FontAwesomeIcon icon={faSchool} />
+            <span style={{marginLeft: 5}}>Undergraduates: {props.college.enrollment}</span>
           </Paper>
         </Grid>
         <Grid item xs>
           <Paper className={classes.paper}>
-            <FaceIcon color="secondary" />
-            Graduate: {props.college.graduates}
+            <FontAwesomeIcon icon={faUserGraduate} />
+            <span style={{marginLeft: 5}}>Graduate: {props.college.graduates}</span>
           </Paper>
         </Grid>
       </Grid>
